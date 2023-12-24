216dcdadbca1ee72df99db7ea4d88ab90faa295f
NotFound1911 <465442062@qq.com>
2023-12-24 11:42:22 +0800

    [feat]etcd服务注册

---
+++ example/registry/client_test.go
@@ -0,0 +1,33 @@
+package registry
+
+import (
+	"context"
+	"github.com/NotFound1911/emicro"
+	"github.com/NotFound1911/emicro/internal/proto/gen"
+	"github.com/NotFound1911/emicro/registry/etcd"
+	"github.com/stretchr/testify/require"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"testing"
+	"time"
+)
+
+func TestClient(t *testing.T) {
+	etcdClient, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{"localhost:2379"},
+	})
+	require.NoError(t, err)
+	r, err := etcd.NewRegistry(etcdClient)
+	require.NoError(t, err)
+
+	client, err := emicro.NewClient(emicro.ClientInsecure(), emicro.ClientWithRegistry(r, time.Second*3))
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	require.NoError(t, err)
+
+	cc, err := client.Dial(ctx, "user-service")
+	require.NoError(t, err)
+	uc := gen.NewUserServiceClient(cc)
+	resp, err := uc.GetById(ctx, &gen.GetByIdReq{Id: 123})
+	require.NoError(t, err)
+	t.Log("resp:", resp)
+}
---
+++ grpc_resolver.go
@@ -0,0 +1,89 @@
+package emicro
+
+import (
+	"context"
+	"github.com/NotFound1911/emicro/registry"
+	"google.golang.org/grpc/resolver"
+	"time"
+)
+
+var _ resolver.Builder = &grpcBuilder{}
+var _ resolver.Resolver = &grpcResolver{}
+
+// grpcBuilder 用于构建Resolver，gRpc维护一个scheme->Builder的映射
+type grpcBuilder struct {
+	r       registry.Registry
+	timeout time.Duration
+}
+
+func NewRegistryBuilder(r registry.Registry, timeout time.Duration) (*grpcBuilder, error) {
+	return &grpcBuilder{r: r, timeout: timeout}, nil
+}
+func (b *grpcBuilder) Build(target resolver.Target, cc resolver.ClientConn,
+	opts resolver.BuildOptions) (resolver.Resolver, error) {
+	r := &grpcResolver{
+		cc:      cc,
+		r:       b.r,
+		target:  target,
+		timeout: b.timeout,
+	}
+	r.resolve()
+	go r.watch()
+	return r, nil
+}
+
+func (b *grpcBuilder) Scheme() string {
+	return "registry"
+}
+
+type grpcResolver struct {
+	target  resolver.Target
+	r       registry.Registry
+	cc      resolver.ClientConn // 存储客户端连接 服务连接的抽象
+	timeout time.Duration
+	close   chan struct{}
+}
+
+// ResolveNow 方法用于立即解析gRPC的目标地址
+func (g *grpcResolver) ResolveNow(options resolver.ResolveNowOptions) {
+	g.resolve()
+}
+
+func (g *grpcResolver) watch() {
+	events, err := g.r.Subscribe(g.target.Endpoint())
+	if err != nil {
+		g.cc.ReportError(err)
+	}
+	for {
+		select {
+		case <-events:
+			g.resolve()
+
+		case <-g.close:
+			return
+		}
+	}
+}
+
+func (g *grpcResolver) resolve() {
+	ctx, cancel := context.WithTimeout(context.Background(), g.timeout)
+	defer cancel()
+	instances, err := g.r.ListServices(ctx, g.target.Endpoint())
+	if err != nil {
+		g.cc.ReportError(err)
+		return
+	}
+	address := make([]resolver.Address, 0, len(instances))
+	for _, si := range instances {
+		address = append(address, resolver.Address{Addr: si.Addr})
+	}
+	err = g.cc.UpdateState(resolver.State{Addresses: address})
+	if err != nil {
+		g.cc.ReportError(err)
+		return
+	}
+}
+
+func (g *grpcResolver) Close() {
+	close(g.close)
+}
